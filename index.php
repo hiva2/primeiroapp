0197184e7997dfb6295a1f0cbfe009827af1dafb
Huan Carlos <201810297@aluno.fjn.edu.br>
2019-05-14 21:41:58 -0300

    primeiroApp

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Meu primeiro app</title>
+</head>
+<body>
+<?php echo "Essa parte está sendo criada pelo PHP";?>
+    
+</body>
+</html>
